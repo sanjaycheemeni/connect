62542985e4ac2fc88dd2075f7a400fc8b7d113fc
Sanjay <56751654+sanjaycheemeni@users.noreply.github.com>
2023-11-05 15:17:58 +0530

    commit

--- ebook/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ebook/view/screenBook.dart';
-import 'package:ebook/view/screenCourseList.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-void main() {
-  runApp(GetMaterialApp(home: CourseList()));
-}
